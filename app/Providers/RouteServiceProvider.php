683a76628bd52670aeaed28fd33901e961c93354
Hendri Heryanto <hendri@senja.co.uk>
2019-04-04 15:29:41 +0700

    add new routes

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -39,7 +39,7 @@ class RouteServiceProvider extends ServiceProvider
 
         $this->mapWebRoutes();
 
-        //
+        $this->mapWebRoutesHendri();
     }
 
     /**
@@ -54,6 +54,13 @@ class RouteServiceProvider extends ServiceProvider
         Route::middleware('web')
              ->namespace($this->namespace)
              ->group(base_path('routes/web.php'));
+    }
+
+    protected function mapWebRoutesHendri()
+    {
+        Route::middleware('web')
+             ->namespace($this->namespace)
+             ->group(base_path('routes/hendri.php'));
     }
 
     /**
